5cd538bb684adc90c46c29d16efcd18fe3d2dfc1
Mohammed_El_Tagger <maltager658@gmail.com>
2025-05-09 23:45:40 +0300

    Update clac.py

--- clac.py
+++ clac.py
@@ -5,7 +5,8 @@ Last_value = int(input("Enter last value: "))
 print(first_value + Last_value)
 print(first_value - Last_value)
 print(first_value * Last_value)
-print(first_value / Last_value)
+print(first_value / Last_value)     
+print(first_value * 2)
 print(first_value ^ Last_value)
 
 
